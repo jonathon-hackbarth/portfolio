3e0e8fbf7763531e7d40ddad72385dee63c8d8a0
jonathon-hackbarth <jonathon.hackbarth@gmail.com>
2025-02-17 23:34:24 -0600

    built hero and projects section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()],
+    },
+});
