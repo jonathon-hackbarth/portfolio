bff21d80b82561b5da4d9ff2f2e3143439647f82
jonathon-hackbarth <jonathon.hackbarth@gmail.com>
2025-02-20 02:21:20 -0600

    vercel deployment dependencies added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,19 @@ import tailwindcss from "@tailwindcss/vite";
 import dotenv from "dotenv";
 import react from '@astrojs/react';
 
+import vercel from "@astrojs/vercel";
+
 dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jonathon-hackbarth.github.io",
   base: "portfolio",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   integrations: [react()],
+  adapter: vercel(),
 });
